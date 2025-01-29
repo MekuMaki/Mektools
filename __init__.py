160c4f12386cfb4be7869e170e1eec2ea0b0eb9a
Alexander Braun <alexanderbraun121@gmail.com>
2025-01-29 14:54:23 +0100

    version change to 1.3.0 pose import is here wuhuu

--- __init__.py
+++ __init__.py
@@ -36,7 +36,7 @@ from .preferences import (
 bl_info = {
     "name": "Mektools",
     "author": "Meku Maki, Shino Mythmaker",
-    "version": (1,2,12),
+    "version": (1,3,0),
     "blender": (4,2),
     "description": "Mektools Addon Structure",
     "category": "Import-Export",
