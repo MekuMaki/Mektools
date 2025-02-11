5adb23946c0fafca36507b60d56e55f45201071c
Alexander Braun <alexanderbraun121@gmail.com>
2025-02-11 19:08:49 +0100

    fixed Collection Names

--- mektools/operators/mekrig_operators.py
+++ mektools/operators/mekrig_operators.py
@@ -42,105 +42,105 @@ class MEKTOOLS_OT_ImportMekrigBase(Operator):
 class MEKTOOLS_OT_ImportMekrigMidlanderMale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_midlander_male"
     bl_label = "Import Midlander Male Rig"
-    collection_name = "Midlander Male"
+    collection_name = "Actor ( Midlander Male )"
     file_name = "Midlander Male.blend"
 
 class MEKTOOLS_OT_ImportMekrigHighlanderMale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_highlander_male"
     bl_label = "Import Highlander Male Rig"
-    collection_name = "Highlander Male"
+    collection_name = "Actor ( Highlander Male )"
     file_name = "Highlander Male.blend"
 
 class MEKTOOLS_OT_ImportMekrigElezenMale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_elezen_male"
     bl_label = "Import Elezen Male Rig"
-    collection_name = "Elezen Male"
+    collection_name = "Actor ( Elezen Male )"
     file_name = "Elezen Male.blend"
 
 class MEKTOOLS_OT_ImportMekrigMiqoteMale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_miqote_male"
     bl_label = "Import Miqote Male Rig"
-    collection_name = "Miqote Male"
+    collection_name = "Actor ( Miqote Male )"
     file_name = "Miqote Male.blend"
 
 class MEKTOOLS_OT_ImportMekrigRoegadynMale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_roegadyn_male"
     bl_label = "Import Roegadyn Male Rig"
-    collection_name = "Roegadyn Male"
+    collection_name = "Actor ( Roegadyn Male )"
     file_name = "Roegadyn Male.blend"
 
 class MEKTOOLS_OT_ImportMekrigAuraMale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_aura_male"
     bl_label = "Import Aura Male Rig"
-    collection_name = "Aura Male"
+    collection_name = "Actor ( Aura Male )"
     file_name = "Aura Male.blend"
 
 class MEKTOOLS_OT_ImportMekrigHrothgarMale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_hrothgar_male"
     bl_label = "Import Hrothgar Male Rig"
-    collection_name = "Hrothgar Male"
+    collection_name = "Actor ( Hrothgar Male )"
     file_name = "Hrothgar Male.blend"
 
 class MEKTOOLS_OT_ImportMekrigVieraMale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_viera_male"
     bl_label = "Import Viera Male Rig"
-    collection_name = "Viera Male"
+    collection_name = "Actor ( Viera Male )"
     file_name = "Viera Male.blend"
 
 # Lalafell Rig Operators (both male and female point to the same file and collection)
 class MEKTOOLS_OT_ImportMekrigLalafellBoth(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_lalafell_both"
     bl_label = "Import Lalafell Rig"
-    collection_name = "Lalafell"
+    collection_name = "Actor ( Lalafell )"
     file_name = "Lalafell.blend"
 
 # Female Rig Operators
 class MEKTOOLS_OT_ImportMekrigMidlanderFemale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_midlander_female"
     bl_label = "Import Midlander Female Rig"
-    collection_name = "Midlander Female"
+    collection_name = "Actor ( Midlander Female )"
     file_name = "Midlander Female.blend"
 
 class MEKTOOLS_OT_ImportMekrigHighlanderFemale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_highlander_female"
     bl_label = "Import Highlander Female Rig"
-    collection_name = "Highlander Female"
+    collection_name = "Actor ( Highlander Female )"
     file_name = "Highlander Female.blend"
 
 class MEKTOOLS_OT_ImportMekrigElezenFemale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_elezen_female"
     bl_label = "Import Elezen Female Rig"
-    collection_name = "Elezen Female"
+    collection_name = "Actor ( Elezen Female )"
     file_name = "Elezen Female.blend"
 
 class MEKTOOLS_OT_ImportMekrigMiqoteFemale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_miqote_female"
     bl_label = "Import Miqote Female Rig"
-    collection_name = "Miqote Female"
+    collection_name = "Actor ( Miqote Female )"
     file_name = "Miqote Female.blend"
 
 class MEKTOOLS_OT_ImportMekrigRoegadynFemale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_roegadyn_female"
     bl_label = "Import Roegadyn Female Rig"
-    collection_name = "Roegadyn Female"
+    collection_name = "Actor ( Roegadyn Female )"
     file_name = "Roegadyn Female.blend"
 
 class MEKTOOLS_OT_ImportMekrigAuraFemale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_aura_female"
     bl_label = "Import Aura Female Rig"
-    collection_name = "Aura Female"
+    collection_name = "Actor ( Aura Female )"
     file_name = "Aura Female.blend"
 
 class MEKTOOLS_OT_ImportMekrigVieraFemale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_viera_female"
     bl_label = "Import Viera Female Rig"
-    collection_name = "Viera Female"
+    collection_name = "Actor ( Viera Female )"
     file_name = "Viera Female.blend"
 
 class MEKTOOLS_OT_ImportMekrigHrothgarFemale(MEKTOOLS_OT_ImportMekrigBase):
     bl_idname = "mektools.import_mekrig_hrothgar_female"
     bl_label = "Import Hrothgar Female Rig"
-    collection_name = "Hrothgar Female"
+    collection_name = "Actor ( Hrothgar Female )"
     file_name = "Hrothgar Female.blend"
 
 # Register and unregister functions
